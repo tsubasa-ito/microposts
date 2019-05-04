0dbe81bbcd1ab8097bf74f214f57fe18d3c4e366
tsubasa.ito <ec2-user@ip-172-31-49-189.ec2.internal>
2019-05-04 14:27:09 +0000

    Confirmation → again

--- app/Http/Controllers/Auth/register.blade.php
+++ app/Http/Controllers/Auth/register.blade.php
@@ -25,7 +25,7 @@
                 </div>
 
                 <div class="form-group">
-                    {!! Form::label('password_confirmation', 'Confirmation') !!}
+                    {!! Form::label('password_confirmation', 'Enter your password again.') !!}
                     {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
                 </div>
 
